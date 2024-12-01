80d1ea985c2f9a6ee739e892de94b09b8b0cd9be
saibal0 <189989929+saibal0@users.noreply.github.com>
2024-12-01 13:06:15 +0000

    https://github.com/saibal0/adventofcode-2024

--- src/01/main.py
+++ src/01/main.py
@@ -8,12 +8,24 @@ def run(file):
     # print(file_content.head(1))
     
     # Question 1
+    '''
+    Pseudo Code:
+        1. Separate columns as individual lists.
+        2. Sort them in ascending order.
+        3. Find difference between each element and take care of negative scenarios and prepare a list.
+        4. Sum the total elements in the list'''
     list_1 = sorted(file_content.list1.to_list())
     list_2 = sorted(file_content.list2.to_list())
     final_list = (abs(np.array(list_2) - np.array(list_1))).tolist()
     print("01. Answer: {}".format(sum(final_list)))
 
     # Question 2
+    '''
+    Pseudo Code:
+        1. Create a new list or reuse the previous list.
+        2. Create a new list by counting the number of records for each element
+             in left list in the right list and multiply by that element
+        3. Sum the total elements in the list'''
     list_1 = file_content.list1.to_list()
     list_2 = file_content.list2.to_list()
 
